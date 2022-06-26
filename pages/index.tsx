39c7b11599dea6db12bc104109b0363ae169042d
musayann <musayann@octangrp.com>
2022-06-26 17:17:24 +0200

    feat: add backend url

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Button from '../components/ui/button'
 const Home: NextPage = () => {
   const [email, setEmail] = useState('')
   const [submitted, setSubmitted] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
   const [department, setDepartment] = useState('')
   const [project, setProject] = useState('')
@@ -15,26 +16,33 @@ const Home: NextPage = () => {
 
   const submit = () => {
     const formData = new FormData();
+    setLoading(true);
 
-		formData.append('file', file);
-    formData.append('email', email);
-    formData.append('department', department);
-    formData.append('project', project);
-    formData.append('user', user);
-    fetch(
-      'http://localhost:8080/upload-csv',
-			{
-				method: 'POST',
-				body: formData,
-			}
-		).then( res => res.blob() )
-    .then( blob => {
-      var file = window.URL.createObjectURL(blob);
-      window.location.assign(file);
-    });
-    setSubmitted(true)
-    console.log(email, user, project, department)
-  }
+    try {
+      formData.append('file', file);
+      formData.append('email', email);
+      formData.append('department', department);
+      formData.append('project', project);
+      formData.append('user', user);
+      fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/upload-csv`,
+        {
+          method: 'POST',
+          body: formData,
+        }
+      ).then(res => res.blob())
+        .then(blob => {
+          var file = window.URL.createObjectURL(blob);
+          window.location.assign(file);
+        });
+      setSubmitted(true)
+    } catch (e: any) {
+      setError(e?.message)
+    } finally {
+      setLoading(false)
+    }
+  };
+
   return (
     <div>
       <Form onSubmit={submit} title="Conversion form" subtitle="Convert your toggl entries to clockify">
@@ -45,7 +53,8 @@ const Home: NextPage = () => {
             <Input name="user" type="text" label="User:" value={user} onChange={(e) => setUser(e.target.value)} />
             <Input name="email" type="email" label="Email:" value={email} onChange={(e) => setEmail(e.target.value)} />
             <Upload name="file" label='Toggl csv export' value={file} onChange={(_file) => setFile(_file)} />
-            <Button onClick={submit}>Upload</Button>
+            <Button onClick={submit} disabled={loading}>{loading ? 'Processing....' : 'Upload'}</Button>
+            {error && <div className='text-red-600'>{error}</div>}
           </>) : (
           <div className='text-center py-12 text-gray-500 flex flex-col space-y-2'>
             <p className='text-4xl'>🎉</p>
