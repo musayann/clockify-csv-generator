78eb6818c0f927b36566e49ca7da983cd5b22964
musayann <musayann@gmail.com>
2023-06-01 21:33:23 +0200

    chore: adjust the site description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
             <Head>
                 <title>Clockify CSV Generator</title>
                 <meta property="og:title" content="Clockify CSV Generator" key="title" />
-                <meta name="description" content="Clockify CSV Generator is a Next.js project that allows users to convert Toggl and awork CSV files into a Clockify-compatible CSV format. The generated CSV file can be manually uploaded to Clockify for importing time entries. Clockify is a popular time tracking tool used by individuals and teams to monitor their work hours." />
+                <meta name="description" content="Clockify CSV Generator is a Next.js project that allows users to convert Toggl and awork CSV files into a Clockify-compatible CSV format. The generated CSV file can be manually uploaded to Clockify for importing time entries." />
             </Head>
             <body>
                 <Main />
